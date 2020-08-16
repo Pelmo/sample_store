76dca90bb32801642733d9a6e1375d07529b7a12
piblo <paulhankins99@gmail.com>
2020-08-15 23:31:52 -0500

    pushy for snelly

--- routes/login.js
+++ routes/login.js
@@ -1,7 +1,25 @@
 const express = require('express')
 const router = express.Router()
+const User = require('../models/User')
 
 router.post('/', (req, res, next) => {
+    const email = req.body.email
+    User.find({email: email}, (err, users) => {
+        if (err) {
+            res.json({
+                confirmation: 'fail',
+                error: err
+            })
+
+            return
+        }
+
+        res.json({
+            confirmation: 'success',
+            user: users
+        })
+    })
+
     res.json({
         data: req.body
     })
