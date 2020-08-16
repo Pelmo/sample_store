3a77d6813174b96fd3bebad6293db6fd8030cc3b
piblo <paulhankins99@gmail.com>
2020-08-15 23:05:58 -0500

    WOW... MONGOOSE

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const express = require('express')
 const path = require('path')
+const mongoose = require('mongoose')
 const home = require('./routes/home')
 const register = require('./routes/register')
+const login = require('./routes/login')
 
+mongoose.connect('mongodb://localhost/sample-store', { useNewUrlParser: true, useUnifiedTopology: true }, (err, data ) => {
+    if (err) {
+        console.log('DB connection Failed')
+        return
+    }
+
+    console.log('DB Connection Success')
+})
 const app = express()
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'hjs')
@@ -12,6 +22,7 @@ app.use(express.urlencoded({extended: false}))
 app.use(express.static(path.join(__dirname, 'public')))
 app.use('/', home)
 app.use('/register', register)
+app.use('/login', login)
 
 app.listen(5000)
 console.log('App running on http://localhost:5000')
