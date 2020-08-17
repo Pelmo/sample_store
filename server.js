7e1dd1bfbec6f5c0558e7f1768be5c54f2159ea8
piblo <paulhankins99@gmail.com>
2020-08-17 03:33:20 -0500

    PASSPORT WOW

--- server.js
+++ server.js
@@ -1,20 +1,32 @@
 const express = require('express')
 const path = require('path')
 const mongoose = require('mongoose')
+const passport = require('passport')
+const session = require('express-session')
+
+const auth = require('./config/auth')(passport) //Import auth.js
 const home = require('./routes/home')
 const register = require('./routes/register')
 const login = require('./routes/login')
+const account = require('./routes/account') //Import the account route
 
 mongoose.connect('mongodb://localhost/sample-store', { useNewUrlParser: true, useUnifiedTopology: true }, (err, data ) => {
     if (err) {
         console.log('DB connection Failed')
         return
     }
-
     console.log('DB Connection Success')
 })
 
 const app = express()
+app.use(session({
+    secret: 'smellybelly',
+    resave: true,
+    saveUninitialized: true
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'hjs')
 
@@ -24,6 +36,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use('/', home)
 app.use('/register', register)
 app.use('/login', login)
+app.use('/account', account)
 
 app.use((err, req, res, next) =>{
     res.render('error', {message: err.message})
