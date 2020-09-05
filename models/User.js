735794608710ce5ce1cc5993578e7808e20e3bc3
piblo <paulhankins99@gmail.com>
2020-09-05 01:37:02 -0500

    yes
    yes

--- models/User.js
+++ models/User.js
@@ -1,10 +1,10 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const User = new mongoose.Schema({
-    email: {type:String, default:''},
-    password: {type:String, default:''},
-    isAdmin: {type:Boolean, default:false},
-    timestamp: {type:Date, default:Date.now}
-})
+  email: { type: String, default: "" },
+  password: { type: String, default: "" },
+  isAdmin: { type: Boolean, default: false },
+  timestamp: { type: Date, default: Date.now },
+});
 
-module.exports = mongoose.model('User', User)
+module.exports = mongoose.model("User", User);
--- routes/account.js
+++ routes/account.js
@@ -1,31 +1,30 @@
-const express = require('express')
-const router = express.Router()
- const items = [
-     {name:'Item 1', description:'', price:10},
-     {name:'Item 2', description:'', price:20},
-     {name:'Item 3', description:'', price:15},
-     {name:'Item 4', description:'', price:50},
-     {name:'Item 5', description:'', price:35},
-     {name:'Item 6', description:'', price:100}
- ]
-router.get('/', (req, res, next) => {
-const user = req.user
-if (user == null) {
-    res.redirect('/')
-    return 
-}
+const express = require("express");
+const router = express.Router();
+const items = [
+  { name: "Item 1", description: "", price: 10 },
+  { name: "Item 2", description: "", price: 20 },
+  { name: "Item 3", description: "", price: 15 },
+  { name: "Item 4", description: "", price: 50 },
+  { name: "Item 5", description: "", price: 35 },
+  { name: "Item 6", description: "", price: 100 },
+];
+router.get("/", (req, res, next) => {
+  const user = req.user;
+  if (user == null) {
+    res.redirect("/");
+    return;
+  }
 
-const data = {
+  const data = {
     user: user,
-    items: items
-}
+    items: items,
+  };
 
-res.render('account', data)
+  res.render("account", data);
+});
 
-})
- 
-router.get('/logout', (req, res, next) => {
-    req.logout()
-    res.redirect('/')
-   })
-module.exports = router
+router.get("/logout", (req, res, next) => {
+  req.logout();
+  res.redirect("/");
+});
+module.exports = router;
